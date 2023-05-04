cec1f435fe6768d64d142aadfece0452b917ea91
PhMachado-ctrl <phmachado555@gmail.com>
2023-05-04 09:46:01 -0300

    modulos-e-coleta

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Adivinhe-o-numero
+# Jogo de Advinhação onde você escreve um número e a maquina gera um numero entre 0 e aquele, e você tem que adivinhar
---
+++ ad_num.py
@@ -0,0 +1,8 @@
+'''Advinhe o número'''
+#Módulos
+import random #Importa a Biblioteca que gera valores aleatórios
+
+#Coletar um número
+print("Seja Bem vindo ao Guess Number!")
+number = input("Digite o número teto do desafio: ")
+
