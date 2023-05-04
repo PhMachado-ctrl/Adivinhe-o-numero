af7f1ac33b45d005751bd030857e92ae6d416540
PhMachado-ctrl <phmachado555@gmail.com>
2023-05-04 09:47:25 -0300

    Verifica-numero

--- ad_num.py
+++ ad_num.py
@@ -6,3 +6,9 @@ import random #Importa a Biblioteca que gera valores aleatórios
 print("Seja Bem vindo ao Guess Number!")
 number = input("Digite o número teto do desafio: ")
 
+#Verifica se é um número
+if number.isdigit():
+   number = int(number) #converte para inteiro
+else:
+   print("Erro: valor inormado não é numérico. Favor execure novamente e informe um número")
+   quit()
